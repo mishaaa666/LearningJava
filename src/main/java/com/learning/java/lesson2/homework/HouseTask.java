e73c207acf754d7b07967a03067af07daf7f947a
Olesia Maidan <olesia@ace.date>
2020-12-07 14:26:40 +0200

    adding homework house task

--- src/main/java/com/learning/java/lesson2/homework/HouseTask.java
+++ src/main/java/com/learning/java/lesson2/homework/HouseTask.java
@@ -4,17 +4,15 @@ public class HouseTask {
     public static void main(String[] args) {
         // 2. Маючи номер квартири, к-сть під*їздів і загальну к-сть квартир порахувати
         // номер під*їзда для заданої квартири
-        int numberOfAppartment = 67;
+        int numberOfAppartment = 109;
         int numberOfEntrance = 9;
         int totalNumberOfAppartment = 972;
 
-        //int numberOfEntranceForAppartment = 5;
         int numberOfAppartmentsInEntrance = totalNumberOfAppartment / numberOfEntrance;
         System.out.println(numberOfAppartmentsInEntrance);
 
-        for (int i = 1; i <= numberOfAppartmentsInEntrance; i++){
-            int numberOfEntranceForAppartment = 1;
+        int numberOfEntranceForAppartment = ( numberOfAppartment - 1 ) / numberOfAppartmentsInEntrance + 1;
+        System.out.println("Number of Entrance For Appartment is = " + numberOfEntranceForAppartment);
 
-        }
     }
 }
