1a8a790722c63fd757e167a159bdf17fbadc8481
Olesia Maidan <olesia@ace.date>
2020-12-10 08:56:02 +0200

    adding homework lesson 3 part 2.1

--- src/main/java/com/learning/java/lesson3/homework/mobileTask/MobilesExample.java
+++ src/main/java/com/learning/java/lesson3/homework/mobileTask/MobilesExample.java
@@ -6,8 +6,16 @@ public class MobilesExample {
                 "6.1`", "Apple A12", 6, 2,
                 "12 MP + 7 MP", 3, 128, "iOS", "Li-Ion");
 
+        Mobile secondMobile = new Mobile("Samsung", "A5", "2013",
+                "5.0`", "1.2GGc", 4, 2,
+                "13 MP + 5 MP", 2, 16, "Android", "Li-Ion");
+
         firstMobile.printMobileProduction();
         firstMobile.printMainCharacteristics();
         firstMobile.printCameraFeatures();
+
+        secondMobile.printMobileProduction();
+        secondMobile.printMainCharacteristics();
+        secondMobile.printCameraFeatures();
     }
 }
